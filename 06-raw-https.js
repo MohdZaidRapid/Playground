69767887fa4e99bc77581cf91603e24b1bc7776f
mohdzaid <mohdzaid@gmail.com>
2022-07-28 18:03:28 +0530

    request https use

--- 06-raw-https.js
+++ 06-raw-https.js
@@ -1,2 +1,20 @@
-const http = require("https")
+const http = require("http")
 
+const url = "http://api.weatherstack.com/current?access_key=241e341ca218f4b15d033eb3149d7406&query=37.8267,-122.4233&units=f"
+
+const request = http.request(url, (response) => {
+    let data = ''
+
+
+    response.on('data', (chunk) => {
+        data = data + chunk.toString();
+
+    })
+
+    response.on("end", () => {
+        const body = JSON.parse(data)
+        console.log(body)
+    })
+})
+
+request.end()
