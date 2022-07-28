77b00385afe853220d6e84e653c04b259ec561b5
mohdzaid <mohdzaid@gmail.com>
2022-07-28 17:34:59 +0530

    intial commit

--- ShortHandproperty.js
+++ ShortHandproperty.js
@@ -7,3 +7,20 @@ const user = {
     location: "Philadelphia"
 }
 console.log(user)
+
+// Object destructuring
+
+const product = {
+    label: "Red notebook",
+    price: 20,
+    stock: 201,
+    salePrice: undefined,
+    rating
+}
+
+const { label, price, stock, salePrice } = product
+
+const arr = ({ label, price, stock, salePrice }) => {
+    console.log(label)
+}
+arr(product)
