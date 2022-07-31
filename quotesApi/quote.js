8e4b4405e09a0f2086b8d96176feaae79f47f80e
mohdzaid <mohdzaid@gmail.com>
2022-07-31 15:01:29 +0530

    quote addded

--- quotesApi/quote.js
+++ quotesApi/quote.js
@@ -1,3 +1,7 @@
+const quote = document.querySelector(".quote");
+
 fetch("https://quotes.rest/qod?language=en")
   .then((res) => res.json())
-  .then((data) => console.log(data.contents.quotes[0].quote));
+  .then((data) => quote.append(data.contents.quotes[0].quote));
+
+// console.log(data.contents.quotes[0].quote)
