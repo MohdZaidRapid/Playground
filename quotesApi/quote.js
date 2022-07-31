0b3a00422fb31d53dddbd7b7ec10e028172864d4
mohdzaid <mohdzaid@gmail.com>
2022-07-31 16:23:43 +0530

    inital commit

---
+++ quotesApi/quote.js
@@ -0,0 +1,5 @@
+const fetch = require("node-fetch");
+
+fetch("https://quotes.rest/qod?language=en")
+  .then((res) => res.json())
+  .then((data) => console.log(data.contents.quotes[0].quote));
--- quotesApi/quote.mjs
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch'
-
-let quote;
-
-fetch("https://quotes.rest/qod?language=en")
-  .then((res) => res.json())
-  .then((data) => (quote = data.contents.quotes[0].quote));
-
-console.log(quote);
