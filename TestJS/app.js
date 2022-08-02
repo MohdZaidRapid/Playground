dec958c4bb54f00328c4005427dd1306eb198691
mohdzaid <mohdzaid@gmail.com>
2022-08-02 13:47:55 +0530

    deepcopy and shallowcopy

---
+++ TestJS/app.js
@@ -0,0 +1,22 @@
+// Shallow and deepCopy
+// let developer1 = {
+//   name: "MohdZaid",
+//   skills: { primary: "Full stack", secondary: "Dev ops" },
+//   calculateAge: function (params) {
+//     return 30;
+//   },
+//   joiningDate: new Date(),
+// };
+
+// // let developer2 = developer1;
+// // developer2.name = "Mohd";
+// let developer2 = { ...developer1 };
+// // Object.assign({}, developer1);
+// // let developer2 = JSON.parse(JSON.stringify(developer1));
+// // developer2.name = "Zaid";
+
+// developer2.skills.primary = "Frontend engineer";
+
+// console.log(developer1);
+// console.log(developer2);
+
